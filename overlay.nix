5abb70f233491cd83431b63343724b59b1e5cf21
Tristan Ross <tristan.ross@midstall.com>
2023-01-26 14:09:17 -0800

    feat: debug and release builds

--- overlay.nix
+++ overlay.nix
@@ -2,29 +2,34 @@
 final: prev:
 with final;
 with lib;
+let
+  mkPackage = mesonBuildType: clang14Stdenv.mkDerivation {
+    pname = "neutron${if mesonBuildType == "release" then "" else "-${mesonBuildType}"}";
+    version = self.shortRev or "dirty";
+
+    src = cleanSource self;
+    outputs = [ "out" "dev" ];
+    inherit mesonBuildType;
+
+    nativeBuildInputs = [ meson ninja ];
+
+    mesonFlags = [
+      "-Dgit-commit=${self.shortRev or "dirty"}"
+      "-Dgit-branch=master"
+    ];
+
+    meta = {
+      description = "Core API for ExpidusOS";
+      homepage = "https://github.com/ExpidusOS/neutron";
+      license = licenses.gpl3Only;
+      maintainers = with maintainers; [ RossComputerGuy ];
+    };
+  };
+in
 rec {
   expidus = prev.expidus.extend (f: p: {
     defaultPackage = f.neutron;
-    neutron = clang14Stdenv.mkDerivation {
-      pname = "neutron";
-      version = self.shortRev or "dirty";
-
-      src = cleanSource self;
-      outputs = [ "out" "dev" ];
-
-      nativeBuildInputs = [ meson ninja ];
-
-      mesonFlags = [
-        "-Dgit-commit=${self.shortRev or "dirty"}"
-        "-Dgit-branch=master"
-      ];
-
-      meta = {
-        description = "Core API for ExpidusOS";
-        homepage = "https://github.com/ExpidusOS/neutron";
-        license = licenses.gpl3Only;
-        maintainers = with maintainers; [ RossComputerGuy ];
-      };
-    };
+    neutron = mkPackage "release";
+    neutron-debug = mkPackage "debugoptimized";
   });
 }
