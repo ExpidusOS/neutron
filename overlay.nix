72a0ea5e24361a32ad07b075d3827c869095968c
Tristan Ross <tristan.ross@midstall.com>
2023-01-26 14:38:01 -0800

    chore: update flake

--- overlay.nix
+++ overlay.nix
@@ -2,34 +2,14 @@
 final: prev:
 with final;
 with lib;
-let
-  mkPackage = mesonBuildType: clang14Stdenv.mkDerivation {
-    pname = "neutron${if mesonBuildType == "release" then "" else if mesonBuildType == "debugoptimized" then "-debug" else "-${mesonBuildType}"}";
-    version = self.shortRev or "dirty";
-
-    src = cleanSource self;
-    outputs = [ "out" "dev" ];
-    inherit mesonBuildType;
-
-    nativeBuildInputs = [ meson ninja ];
-
-    mesonFlags = [
-      "-Dgit-commit=${self.shortRev or "dirty"}"
-      "-Dgit-branch=master"
-    ];
-
-    meta = {
-      description = "Core API for ExpidusOS";
-      homepage = "https://github.com/ExpidusOS/neutron";
-      license = licenses.gpl3Only;
-      maintainers = with maintainers; [ RossComputerGuy ];
-    };
-  };
-in
 rec {
   expidus = prev.expidus.extend (f: p: {
     defaultPackage = f.neutron;
-    neutron = mkPackage "release";
-    neutron-debug = mkPackage "debugoptimized";
+
+    neutron = p.neutron.mkPackage {
+      rev = self.shortRev or "dirty";
+      src = cleanSource self;
+      buildType = "release";
+    };
   });
 }
