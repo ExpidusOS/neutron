ae5a74bbbd195865e0db092da62b8ce04e63475d
Tristan Ross <tristan.ross@midstall.com>
2023-01-28 17:21:25 -0800

    chore: update flake

--- overlay.nix
+++ overlay.nix
@@ -6,17 +6,10 @@ rec {
   expidus = prev.expidus.extend (f: p: {
     defaultPackage = f.neutron;
 
-    neutron = (p.neutron.mkPackage {
+    neutron = p.neutron.mkPackage {
       rev = self.shortRev or "dirty";
       src = cleanSource self;
       buildType = "release";
-    }).overrideAttrs (s: {
-      nativeBuildInputs = (s.nativeBuildInputs or []) ++ (with buildPackages; [
-        gtk-doc libxslt docbook_xsl docbook_xml_dtd_412
-        docbook_xml_dtd_42 docbook_xml_dtd_43
-      ]);
-      buildInputs = (s.buildInputs or []) ++ [ flutter-engine ];
-      doCheck = true;
-    });
+    };
   });
 }
