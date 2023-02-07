8670ac32f1a9411de025e78c6198e2b956797b1c
Tristan Ross <tristan.ross@midstall.com>
2023-02-07 13:12:24 -0800

    feat(platform): add more headers to main platform header

--- platform/include/neutron/platform.h
+++ platform/include/neutron/platform.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#include <neutron/platform/device.h>
+#include <neutron/platform/device-enum.h>
 #include <neutron/platform/platform.h>
+#include <neutron/platform/process.h>
+#include <neutron/platform/signal.h>
