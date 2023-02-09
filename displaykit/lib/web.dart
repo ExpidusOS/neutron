9fb070dd29f6bc7d602572fe0b8f94527b544770
Tristan Ross <tristan.ross@midstall.com>
2023-02-08 19:40:47 -0800

    feat(displaykit): add dart bindings

---
+++ displaykit/lib/web.dart
@@ -0,0 +1,21 @@
+import 'package:web_ffi/web_ffi.dart';
+import 'package:web_ffi/web_ffi_modules.dart';
+import 'path.dart';
+
+Module? _module;
+
+Future<void> init() async {
+  if (_module == null) {
+    Memory.init();
+    _module = await EmscriptenModule.process('neutron-displaykit');
+  }
+}
+
+DynamicLibrary open() {
+  Module? m = _module;
+  if (m != null) {
+    return new DynamicLibrary.fromModule(m);
+  }
+
+  throw new StateError('Must call init() before running open() for Neutron DisplayKit')
+}
