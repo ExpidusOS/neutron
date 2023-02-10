51f3291c5cb60df073b4f0c4df85f32d0485f20f
Tristan Ross <tristan.ross@midstall.com>
2023-02-09 17:29:10 -0800

    chore: update flake

--- overlay.nix
+++ overlay.nix
@@ -7,8 +7,15 @@ rec {
     let
       makeOverride = p: (p.neutron.mkPackage {
         rev = self.shortRev or "dirty";
-        src = cleanSource self;
+        src = cleanSourceWith {
+          filter = name: type:
+            let
+              baseName = baseNameOf (toString name);
+            in !(hasSuffix ".nix" baseName);
+          src = cleanSource self;
+        };
         buildType = "release";
+        vendorHash = "sha256-4fupDNi+lgLSQCuUlIDWJCTfXh8OD93hxuuwkuVoQww=";
       }).overrideAttrs (s: {
         nativeBuildInputs = s.nativeBuildInputs ++ (with buildPackages; [ flutter dart ]);
         buildInputs = (s.buildInputs or []) ++ [ xorg.libxcb wlroots wayland wayland-protocols udev libxkbcommon ];
