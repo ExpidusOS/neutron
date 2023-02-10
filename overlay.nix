7cda7c85af49accca9625f5c8f5297d2c0e243e3
Tristan Ross <tristan.ross@midstall.com>
2023-02-09 17:37:12 -0800

    feat: will not be vendoring, remove unneeded dart deps

--- overlay.nix
+++ overlay.nix
@@ -5,22 +5,11 @@ with lib;
 rec {
   expidus = prev.expidus.extend (f: p:
     let
-      makeOverride = p: (p.neutron.mkPackage {
+      makeOverride = p: p.neutron.mkPackage {
         rev = self.shortRev or "dirty";
-        src = cleanSourceWith {
-          filter = name: type:
-            let
-              baseName = baseNameOf (toString name);
-            in !(hasSuffix ".nix" baseName);
-          src = cleanSource self;
-        };
+        src = cleanSource self;
         buildType = "release";
-        vendorHash = "sha256-4fupDNi+lgLSQCuUlIDWJCTfXh8OD93hxuuwkuVoQww=";
-      }).overrideAttrs (s: {
-        nativeBuildInputs = s.nativeBuildInputs ++ (with buildPackages; [ flutter dart ]);
-        buildInputs = (s.buildInputs or []) ++ [ xorg.libxcb wlroots wayland wayland-protocols udev libxkbcommon ];
-        mesonFlags = (s.mesonFlags or []) ++ [ "-Dbootstrap=false" "-Dflutter-engine=${flutter-engine}/lib/flutter/out/release" ];
-      });
+      };
     in {
       defaultPackage = f.neutron;
       neutron = makeOverride p;
