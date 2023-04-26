2ddb0c0d6e5b0340ecb2371ddf824e122f6bf822
Tristan Ross <tristan.ross@midstall.com>
2023-04-25 21:51:29 -0700

    chore: created backing store

--- src/neutron/graphics/renderer/egl.zig
+++ src/neutron/graphics/renderer/egl.zig
@@ -55,6 +55,11 @@ const Impl = struct {
       .flutter_context = undefined,
       .tex_coord_buffer = undefined,
       .quad_vert_buffer = undefined,
+      .pages = [_]Page { Page.init(self) } ** 2,
+      .curr_page = 0,
+      .procs = .{
+        .glDrawBuffers = try api.tryResolve(?*const fn (n: c.GLsizei, bufs: [*c]const c.GLenum) callconv(.C) void, "glDrawBuffers"),
+      },
     };
     errdefer self.base.unref();
 
@@ -117,6 +122,8 @@ const Impl = struct {
       .flutter_context = self.flutter_context,
       .tex_coord_buffer = self.tex_coord_buffer,
       .quad_vert_buffer = self.quad_vert_buffer,
+      .pages = self.pages,
+      .curr_page = self.curr_page,
     };
   }
 
@@ -132,6 +139,75 @@ const Impl = struct {
   }
 };
 
+const PageTexture = struct {
+  size: @Vector(2, usize),
+  fbo: c.GLuint,
+  tex: c.GLuint,
+
+  pub fn init(renderer: *Self, size: @Vector(2, usize), make_fbo: bool) !PageTexture {
+    var fbo: c.GLuint = 0;
+    if (make_fbo) {
+      c.glGenFramebuffers(1, &fbo);
+      c.glBindFramebuffer(c.GL_FRAMEBUFFER, fbo);
+    }
+
+    var tex: c.GLuint = 0;
+    c.glGenTextures(1, &tex);
+    c.glBindTexture(c.GL_TEXTURE_2D, tex);
+    c.glTexImage2D(c.GL_TEXTURE_2D, 0, c.GL_BGRA_EXT, @intCast(c_int, size[0]), @intCast(c_int, size[1]), 0, c.GL_BGRA_EXT, c.GL_UNSIGNED_BYTE, null);
+    c.glTexParameteri(c.GL_TEXTURE_2D, c.GL_TEXTURE_MIN_FILTER, c.GL_LINEAR);
+    c.glTexParameteri(c.GL_TEXTURE_2D, c.GL_TEXTURE_MAG_FILTER, c.GL_LINEAR);
+
+    if (make_fbo) {
+      c.glFramebufferTexture2D(c.GL_FRAMEBUFFER, c.GL_COLOR_ATTACHMENT0, c.GL_TEXTURE_2D, tex, 0);
+      
+      var bufs = &[_]c.GLenum { c.GL_COLOR_ATTACHMENT0 };
+      renderer.procs.glDrawBuffers(1, bufs);
+
+      c.glBindFramebuffer(c.GL_FRAMEBUFFER, 0);
+    }
+
+    c.glBindTexture(c.GL_TEXTURE_2D, 0);
+
+    const err = c.glGetError();
+    if (err != 0) return error.glError;
+
+    return .{
+      .tex = tex,
+      .fbo = fbo,
+      .size = size,
+    };
+  }
+};
+
+const Page = struct {
+  renderer: *Self,
+  textures: std.ArrayList(PageTexture),
+  unused_textures: std.ArrayList(PageTexture),
+
+  pub fn init(renderer: *Self) Page {
+    return .{
+      .renderer = renderer,
+      .textures = std.ArrayList(PageTexture).init(renderer.type.allocator),
+      .unused_textures = std.ArrayList(PageTexture).init(renderer.type.allocator),
+    };
+  }
+
+  pub fn getTexture(self: *Page, size: @Vector(2, usize), make_fbo: bool) !*PageTexture {
+    for (self.unused_textures.items, 0..) |*page_texture, i| {
+      if (page_texture.size[0] == size[0] and page_texture.size[1] == size[1]) {
+        const index = self.textures.items.len;
+        try self.textures.append(self.unused_textures.swapRemove(i));
+        return &self.textures.items[index];
+      }
+    }
+
+    const index = self.textures.items.len;
+    try self.textures.append(try PageTexture.init(self.renderer, size, make_fbo));
+    return &self.textures.items[index];
+  }
+};
+
 pub const Type = elemental.Type(Self, *hardware.device.Gpu, Impl);
 
 @"type": Type,
@@ -142,18 +218,46 @@ context: c.EGLContext,
 flutter_context: c.EGLContext,
 tex_coord_buffer: c.GLuint,
 quad_vert_buffer: c.GLuint,
+pages: [2]Page,
+curr_page: usize,
+procs: struct {
+  glDrawBuffers: *const fn (n: c.GLsizei, bufs: [*c]const c.GLenum) callconv(.C) void,
+},
 compositor: flutter.c.FlutterCompositor = .{
   .struct_size = @sizeOf(flutter.c.FlutterCompositor),
   .user_data = null,
   .avoid_backing_store_cache = true,
   .create_backing_store_callback = (struct {
     fn callback(config: [*c]const flutter.c.FlutterBackingStoreConfig, backing_store_out: [*c]flutter.c.FlutterBackingStore, _self: ?*anyopaque) callconv(.C) bool {
-      _ = config;
-      _ = backing_store_out;
-
       const self = Type.fromOpaque(_self.?);
-      _ = self;
-      return false;
+      const page = &self.pages[self.curr_page];
+      const page_texture = page.getTexture(.{ std.math.lossyCast(usize, config.*.size.width), std.math.lossyCast(usize, config.*.size.height) }, false) catch return false;
+
+      backing_store_out.* = .{
+        .struct_size = @sizeOf(flutter.c.FlutterBackingStore),
+        .user_data = page_texture,
+        .type = flutter.c.kFlutterBackingStoreTypeOpenGL,
+        .did_update = true,
+        .unnamed_0 = .{
+          .open_gl = .{
+            .type = flutter.c.kFlutterOpenGLTargetTypeTexture,
+            .unnamed_0 = .{
+              .texture = .{
+                .target = c.GL_TEXTURE_2D,
+                .name = page_texture.tex,
+                .width = page_texture.size[0],
+                .height = page_texture.size[1],
+                .format = c.GL_BGRA_EXT,
+                .user_data = page_texture,
+                .destruction_callback = (struct {
+                  fn callback(_: ?*anyopaque) callconv(.C) void {}
+                }).callback,
+              },
+            },
+          },
+        },
+      };
+      return true;
     }
   }).callback,
   .collect_backing_store_callback = (struct {
