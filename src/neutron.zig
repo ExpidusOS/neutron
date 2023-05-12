5d7baa032d39d04365141c13b2293dc7e85e918e
Tristan Ross <tristan.ross@midstall.com>
2023-05-11 19:52:50 -0700

    chore: update flutter and zig

--- src/neutron.zig
+++ src/neutron.zig
@@ -2,6 +2,7 @@ pub const config = @import("neutron-config");
 
 pub const displaykit = @import("neutron/displaykit.zig");
 pub const elemental = @import("neutron/elemental.zig");
+pub const flutter = @import("neutron/flutter.zig");
 pub const graphics = @import("neutron/graphics.zig");
 pub const hardware = @import("neutron/hardware.zig");
 pub const logging = @import("neutron/logging.zig");
